4073af52f61182dacc539071804dd19545fa43af
geumybae <geumybae@gmail.com>
2021-09-19 20:41:40 +0900

    2

---
+++ src/baekjoon/Q1000.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Q1000 {
+ 
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+}
